8444e7377b50ac25221cfdc592965703182bc619
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-28 02:37:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/quick-cache/tags/2.3@352820 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/translations.inc.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit ("Do not access this file directly.");
+/**/
+if (!class_exists ("c_ws_plugin__qcache_translations"))
+	{
+		class c_ws_plugin__qcache_translations
+			{
+				/*
+				Mangles internal translations.
+				Attach to: add_filter("gettext");
+				*/
+				public static function translation_mangler ($translated = FALSE, $original = FALSE, $domain = FALSE)
+					{
+						return $translated;
+					}
+			}
+	}
+?>
