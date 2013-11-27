1b942dc62a1acec9a56b9b44fd9f178c19f98cd7
WebSharks <WebSharks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-27 19:56:30 +0000

    Uploading new release. See `readme.txt` changelog.
    git-svn-id: https://plugins.svn.wordpress.org/quick-cache/tags/131127@811579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ quick-cache.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Version: 131127
+Text Domain: quick-cache
+Plugin Name: Quick Cache
+
+Author: s2Member® / WebSharks, Inc.
+Author URI: http://www.s2member.com
+
+Plugin URI: http://www.websharks-inc.com/product/quick-cache/
+Description: WordPress advanced cache plugin; speed without compromise!
+
+Speed up your site (BIG time!) — Quick Cache provides reliable page caching for WordPress. Easy-to-use (very simple installation).
+*/
+if(!defined('WPINC')) // MUST have WordPress.
+	exit('Do NOT access this file directly: '.basename(__FILE__));
+
+if(version_compare(PHP_VERSION, '5.3', '<')) add_action('all_admin_notices', function ()
+	{
+		echo __('<div class="error"><p>Plugin NOT active. This version of Quick Cache requires PHP v5.3+.</p></div>', 'quick-cache');
+	});
+else require_once dirname(__FILE__).'/quick-cache.inc.php';
